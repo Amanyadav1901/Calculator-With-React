7ab703f9b70aba534e3f02486b1dc8afb41b4010
Amanyadav1901 <amanyadav_co20b2_51@dtu.ac.in>
2023-04-24 16:19:32 +0530

    project completed 24.04

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,3 +1,5 @@
+import { useContext } from "react";
+import {CalcContext} from "../context/CalcContext";
 
 const getStyleName = btn => {
     const className = {
@@ -11,8 +13,104 @@ const getStyleName = btn => {
 }
 
 const Button = ({value}) => {
+
+    const { calc , setCalc } = useContext(CalcContext)
+
+    const commaClick = () => {
+        setCalc({
+            ...calc,
+            num: !calc.num.toString().includes('.') ? calc.num + value : calc.num 
+        })
+    }
+
+    const resetClick = () => {
+        setCalc({sign: '', num: 0, res: 0})
+    }
+
+    const handleClickButton = () => {
+        const numberString = value.toString()
+
+        let numberValue;
+
+        if(numberString === '0' && calc.num === 0){
+            numberValue = "0"
+        }
+        else{
+            numberValue = Number(calc.num + numberString)
+        }
+
+        setCalc({
+            ...calc,
+            num: numberValue
+        })
+    }
+
+    const signClick = () => {
+        setCalc({
+            sign: value,
+            res: !calc.res && calc.num ? calc.num : calc.res,
+            num: 0
+        })
+    }
+
+    const equalsClick = () => {
+
+        if(calc.res && calc.num) {
+            const math = (a,b,sign) => {
+                const result = {
+                    '+': (a,b)=> a+b,
+                    '-': (a,b)=> a-b,
+                    'x': (a,b)=> a*b,
+                    '/': (a,b)=> a/b,
+                }
+                return result[sign](a,b);
+            }
+
+            setCalc({
+                res: math(calc.res, calc.num , calc.sign),
+                sign: '',
+                num: 0
+            })
+        }
+        
+    }
+
+    const percentClick = () => {
+        setCalc({
+            num: (calc.num / 100),
+            res: (calc.res / 100),
+            sign: ''
+        })
+    }
+
+    const invertClick = () => {
+        setCalc({
+            num: calc.num ? calc.num * -1 : 0,
+            res: calc.res ? calc.res * -1 : 0,
+            sign: ''
+        })
+    }
+    const handleBtnClick = ()=> {
+        const results = {
+            '.': commaClick,
+            'C': resetClick,
+            '/': signClick,
+            'x': signClick,
+            '-': signClick,
+            '+': signClick,
+            '=': equalsClick,
+            '%': percentClick,
+            '+-': invertClick,
+        } 
+        if(results[value]) {
+            return results[value]()
+        }
+        else{
+            return handleClickButton()
+        }
+    }
   return (
-    <button className={`${getStyleName(value)} button`}>{value}</button>
+    <button onClick={handleBtnClick} className={`${getStyleName(value)} button`}>{value}</button>
   )
 }
 
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,8 +1,18 @@
+import { useContext } from "react";
+import { CalcContext } from "../context/CalcContext";
+import { Textfit } from "react-textfit";
 
 
-function Screen() {
-  return (
-    <div className="screen">82945792</div>
+const Screen = () => {
+
+  const {calc} = useContext(CalcContext);
+
+
+
+  return(
+    <Textfit className="screen" max={70}
+      mode = "single"
+    >{calc.num ? calc.num : calc.res}</Textfit>
   )
 }
 
